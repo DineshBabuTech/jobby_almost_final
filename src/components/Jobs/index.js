e5e662f3fe28396621c3256f2e61c8df6a944096
DineshBabuTech <dineshbabu.tech.16@gmail.com>
2023-03-17 06:00:20 +0000

    solution

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,12 +1,13 @@
 import {Component} from 'react'
 
+import Loader from 'react-loader-spinner'
+import Cookies from 'js-cookie'
+
 import {BsSearch} from 'react-icons/bs'
 
 import Header from '../Header'
-
+import JobCard from '../JobCard'
 import Profile from '../Profile'
-
-import AllJobs from '../AllJobs'
 
 import './index.css'
 
@@ -48,19 +49,142 @@ const salaryRangesList = [
   },
 ]
 
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
 class Jobs extends Component {
-  state = {searchInput: '', salaryRangeId: ''}
+  state = {
+    searchInput: '',
+    salaryRangeId: '',
+    jobsList: [],
+    employmentTypeId: '',
+    apiStatus: apiStatusConstants.initial,
+  }
+
+  componentDidMount() {
+    this.getJobs()
+  }
+
+  getJobs = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+    const {employmentTypeId, searchInput, salaryRangeId} = this.state
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = `https://apis.ccbp.in/jobs?employment_type=${employmentTypeId}&minimum_package=${salaryRangeId}&title_search=${searchInput}`
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    const fetchedData = await response.json()
+    if (response.ok === true) {
+      const updatedData = fetchedData.jobs.map(job => ({
+        companyLogoUrl: job.company_logo_url,
+        employmentType: job.employment_type,
+        id: job.id,
+        jobDescription: job.job_description,
+        location: job.location,
+        packagePerAnnum: job.package_per_annum,
+        rating: job.rating,
+        title: job.title,
+      }))
+      this.setState({
+        jobsList: updatedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  renderFailureView = () => (
+    <div className="failure-view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
+        alt="failure view"
+        className="jobs-failure-img"
+      />
+      <h1 className="jobs-failure-heading-text">Oops! Something Went Wrong</h1>
+      <p className="jobs-failure-description">
+        We are having some trouble processing your request. Please try again.
+      </p>
+      <button onClick={this.againGetJobs()} className="retry-btn" type="button">
+        Retry
+      </button>
+    </div>
+  )
+
+  renderJobsListView = () => {
+    const {jobsList} = this.state
+    const shouldShowJobsList = jobsList.length > 0
+
+    return shouldShowJobsList ? (
+      <ul className="jobs-list">
+        {jobsList.map(job => (
+          <JobCard jobDetails={job} key={job.id} />
+        ))}
+      </ul>
+    ) : (
+      <div className="no-jobs-view">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
+          className="no-jobs-img"
+          alt="no jobs"
+        />
+        <h1 className="no-jobs-heading">No Jobs Found</h1>
+        <p className="no-jobs-description">
+          We could not find any jobs. Try other filters.
+        </p>
+      </div>
+    )
+  }
+
+  renderLoadingView = () => (
+    <div data-testid="loader" className="jobs-loader-container">
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+    </div>
+  )
+
+  renderAllJobs = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderJobsListView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  againGetJobs = () => {
+    this.getJobs()
+  }
 
   getSearchInput = event => {
     this.setState({searchInput: event.target.value})
   }
 
-  changeSalaryRangeId = event => {
-    this.setState({salaryRangeId: event.target.value})
+  getSearchEntry = () => {
+    const {searchInput} = this.state
+    this.setState({searchInput}, this.getJobs)
   }
 
   render() {
-    const {searchInput, salaryRangeId} = this.state
+    const {searchInput} = this.state
+
     return (
       <>
         <Header />
@@ -68,12 +192,14 @@ class Jobs extends Component {
           <div className="responsive-cont">
             <div className="s-search-box">
               <input
+                onChange={this.getSearchInput}
                 className="s-search"
                 type="search"
                 value={searchInput}
                 placeholder="Search"
               />
               <button
+                onClick={this.getSearchEntry}
                 data-testid="searchButton"
                 type="button"
                 className="search-icon"
@@ -108,7 +234,6 @@ class Jobs extends Component {
                     name="salary range"
                     className="checkbox"
                     type="radio"
-                    value={range.salaryRangeId}
                     id={range.salaryRangeId}
                   />
                   <label htmlFor={range.salaryRangeId} className="labe-text">
@@ -128,6 +253,7 @@ class Jobs extends Component {
                 placeholder="Search"
               />
               <button
+                onClick={this.getSearchEntry}
                 data-testid="searchButton"
                 type="button"
                 className="search-icon"
@@ -135,7 +261,7 @@ class Jobs extends Component {
                 <BsSearch />
               </button>
             </div>
-            <AllJobs salaryRangeId={salaryRangeId} searchInput={searchInput} />
+            {this.renderAllJobs()}
           </div>
         </div>
       </>
